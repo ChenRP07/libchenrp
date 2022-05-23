39632730e0b640842aee5046601d03b6d4150ac6
ChenRP07 <chenruopeng042918@163.com>
2022-05-23 15:44:02 +0800

    dll

--- external.sh
+++ external.sh
@@ -1,3 +1,8 @@
 cp 3rd/libjpeg-turbo/turbojpeg.h ./include/
-mkdir ./dll
+mkdir dll
+cd 3rd/libjpeg-turbo/
+mkdir build
+cd build
+cmake -G"Unix Makefiles" ..
+make
 cp 3rd/libjpeg-turbo/build/libturbojpeg.so.0 ./dll/
--- include/coder.h
+++ include/coder.h
@@ -1,3 +1,10 @@
+/***
+ * @Author: ChenRP07
+ * @Date: 2022-05-22 16:59:30
+ * @LastEditTime: 2022-05-22 17:35:40
+ * @LastEditors: ChenRP07
+ * @Description:
+ */
 #ifndef _VOLUMETRIC_VIDEO_CODER_H_
 #define _VOLUMETRIC_VIDEO_CODER_H_
 
@@ -13,12 +20,16 @@
 
 namespace pco {
 namespace coder {
-    extern size_t MortonIndex2XY(const std::vector<uint8_t>&, std::vector<std::vector<uint8_t>>&);
-    extern void   MortonXY2Index(const std::vector<std::vector<uint8_t>>&, std::vector<uint8_t>&, const size_t);
-    extern void   TurboJpegEncoder(const std::vector<std::vector<uint8_t>>&, const std::string&, const int);
-    extern void   TurboJpegDecoder(std::vector<std::vector<uint8_t>>&, const std::string&);
-    extern int    ZstdEncoder();
-    extern int    ZstdDecoder();
-}  // namespace coder
-}  // namespace pco
+extern size_t MortonIndex2XY(const std::vector<uint8_t> &,
+                             std::vector<std::vector<uint8_t>> &);
+extern void MortonXY2Index(const std::vector<std::vector<uint8_t>> &,
+                           std::vector<uint8_t> &, const size_t);
+extern void TurboJpegEncoder(const std::vector<std::vector<uint8_t>> &,
+                             const std::string &, const int);
+extern void TurboJpegDecoder(std::vector<std::vector<uint8_t>> &,
+                             const std::string &);
+extern int ZstdEncoder();
+extern int ZstdDecoder();
+} // namespace coder
+} // namespace pco
 #endif
