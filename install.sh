adfd946bb972183dbecc9dbe8fd0bd03b4914c01
ChenRP07 <chenruopeng042918@163.com>
2022-05-27 10:34:17 +0800

    install

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 ###
  # @Author: ChenRP07
  # @Date: 2022-05-27 10:13:06
- # @LastEditTime: 2022-05-27 10:13:07
+ # @LastEditTime: 2022-05-27 10:32:55
  # @LastEditors: ChenRP07
  # @Description: 
 ### 
 echo "liboctree will be installed to $1"
-cp ./include/* $1/incldue/
+cp ./include/* $1/include/
 cp ./release/* $1/dll/
