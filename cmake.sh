02e7c70346ceec568f7ef49d45cd24709bcb3f34
ChenRP07 <chenruopeng042918@163.com>
2022-05-27 10:43:14 +0800

    cmake

--- cmake.sh
+++ cmake.sh
@@ -1,11 +1,11 @@
 ###
  # @Author: ChenRP07
  # @Date: 2022-05-26 17:11:47
- # @LastEditTime: 2022-05-27 10:39:30
+ # @LastEditTime: 2022-05-27 10:42:58
  # @LastEditors: ChenRP07
  # @Description: 
 ### 
-if [! -d ./build/ ]; then
+if [ ! -d ./build/ ]; then
     mkdir build
     cd build
     cmake -DINIT=Y -DCMAKE_BUILD_TYPE=Release ..
