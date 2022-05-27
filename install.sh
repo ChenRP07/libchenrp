bb914c86e59e7c9b392e6bc7bd8e981b04c40aea
ChenRP07 <chenruopeng042918@163.com>
2022-05-27 10:33:40 +0800

    install

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 ###
  # @Author: ChenRP07
  # @Date: 2022-05-27 09:35:52
- # @LastEditTime: 2022-05-27 09:35:53
+ # @LastEditTime: 2022-05-27 10:33:08
  # @LastEditors: ChenRP07
  # @Description: 
 ### 
 echo "libregistration will be installed to $1"
-cp ./include/* $1/incldue/
+cp ./include/* $1/include/
 cp ./release/* $1/dll/
