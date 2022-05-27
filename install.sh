55045f67e1d0c1feb9775dddb7fad07a635f9d6e
ChenRP07 <chenruopeng042918@163.com>
2022-05-27 10:34:07 +0800

    install

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 ###
  # @Author: ChenRP07
  # @Date: 2022-05-26 17:13:29
- # @LastEditTime: 2022-05-26 17:14:43
+ # @LastEditTime: 2022-05-27 10:32:44
  # @LastEditors: ChenRP07
  # @Description: install
 ### 
 
 echo "libcluster will be installed to $1"
-cp ./include/* $1/incldue/
+cp ./include/* $1/include/
 cp ./release/* $1/dll/
