35adb4dde2cf3df85c72fc1651c7606d7cdee0ee
ChenRP07 <chenruopeng042918@163.com>
2022-05-27 10:52:48 +0800

    build:

--- cmake.sh
+++ cmake.sh
@@ -1,8 +1,10 @@
 source ./external.sh
-if [ ! -d ./build ]; then
-	mkdir build
+if [! -d ./build/ ]; then
+    mkdir build
+    cd build
+    cmake -DINIT=Y -DCMAKE_BUILD_TYPE=Release ..
+else
+    cd build
 fi
-cd build
-cmake -DINIT=Y -DCMAKE_BUILD_TYPE=Release ..
 make
 cd ../
